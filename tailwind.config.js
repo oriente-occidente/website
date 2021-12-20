d2fbdc98d730b3e1e03df5e0e0bbedc917028b2c
Guido Squillace <g.squillace@cantierecreativo.net>
2021-12-20 22:49:07 +0100

    Resolve confilct in global.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,23 +5,49 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: 'var(--primary)',
-        secondary: 'var(--secondary)',
-        violet: 'var(--violet)',
-        'violet-light': 'var(--violet-light)',
-        pink: 'var(--pink)',
-        cyan: 'var(--cyan)',
-        blue: 'var(--blue)',
-        green: 'var(--green)',
-        red: 'var(--red)',
+        green: '#81845C',
+        black: {
+          DEFAULT: '#000000',
+          light: '#424242',
+          medium: '#979797',
+          transparent: 'rgb(0 0 0 / 60%)',
+        },
+        gray: {
+          DEFAULT: '#E5E5E5',
+          light: '#F7F7F7',
+        }
       },
-      textColor: {
-        base: 'var(--text-base)',
-        primary: 'var(--text-primary)',
-        secondary: 'var(--text-secondary)',
+      fontSize: {
+        xs: ['16px', '20px'],
+        sm: ['18px', '28px'],
+        base: ['23px', '38px'],
+        lg: ['30px', '40px'],
+        xl: ['35px', '43px'],
+        '2xl': ['40px', '52px'],
+        '3xl': ['100px', '110px'],
+        '4xl': ['140px', '150px'],
+      },
+      fontFamily: {
+        'serif': ['Poppins', 'sans-serif'],
+        'sans': ['Poppins', 'sans-serif']
       },
     },
   },
+  corePlugins: {
+    container: false
+  },
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '100%',
+          '@screen 2xl': {
+            maxWidth: '1380px',
+          },
+        }
+      })
+    }
+  ],
   variants: {},
   plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
 };
