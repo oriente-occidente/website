bcc94a478480e80e2dc0fe9069be181abd7dd799
Lorezz <point.point@gmail.com>
2021-12-27 18:51:40 +0100

    queries + structured content

--- components/MyStructuredContent.js
+++ components/MyStructuredContent.js
@@ -1,21 +1,14 @@
 import { StructuredText, renderRule } from 'react-datocms';
-import {
-  isList,
-  isListItem,
-  isBlockquote,
-  isCode,
-  isHeading,
-  isParagraph,
-  isLink,
-} from 'datocms-structured-text-utils';
+import { isBlockquote } from 'datocms-structured-text-utils';
 import Link from 'next/link';
 
 import BlockQuote from 'components/BlockQuote';
-import CodeHilight from 'components/CodeHilight';
-import Cta from 'components/Cta';
 import Gallery from 'components/Gallery';
+// import VideoBlock from 'components/VideoBlock';
 
-const StructuredContent = ({ content }) => {
+import { resolveLinkById } from 'lib/api';
+
+const StructuredContent = ({ locale, content }) => {
   const renderBlock = (record) => {
     console.log('block', record.__typename);
     switch (record.__typename) {
@@ -25,21 +18,19 @@ const StructuredContent = ({ content }) => {
             <Gallery images={record.images} />
           </div>
         );
-      case 'LinkRecord':
+      case 'ImageBlockRecord':
         return (
           <div key={record.id}>
             <pre>{JSON.stringify(record)}</pre>
           </div>
         );
-      case 'CtaRecord':
+      case 'VideoBlockRecord':
         return (
           <div key={record.id}>
-            <Cta cta={record} />
+            video
+            {/* <VideoBlock cta={record} /> */}
           </div>
         );
-
-      case 'EmbeddedVideoRecord':
-      case 'InternalVideoRecord':
       default:
         return null;
     }
@@ -55,88 +46,23 @@ const StructuredContent = ({ content }) => {
         }}
         renderLinkToRecord={({ record, children, transformedMeta }) => {
           // console.log('link', record.__typename);
-          switch (record.__typename) {
-            case 'PostRecord':
-              return (
-                <Link
-                  {...transformedMeta}
-                  href={`/blog/${record.slug}`}
-                  key={record.id}
-                  passHref={true}
-                >
-                  <a>{children}</a>
-                </Link>
-              );
-            case 'TagRecord':
-              return (
-                <Link
-                  {...transformedMeta}
-                  href={`/tag/${record.slug}`}
-                  key={record.id}
-                  passHref={true}
-                >
-                  <a>{children}</a>
-                </Link>
-              );
-            case 'PageRecord':
-              return (
-                <Link
-                  {...transformedMeta}
-                  href={`/${record.slug}`}
-                  key={record.id}
-                  passHref={true}
-                >
-                  <a> {children}</a>
-                </Link>
-              );
-            default:
-              return null;
-          }
+          const resolved = resolveLinkById(locale, record.id);
+          return (
+            <Link
+              {...transformedMeta}
+              href={resolved}
+              key={record.id}
+              locale={locale}
+            >
+              <a>{children}</a>
+            </Link>
+          );
         }}
         renderBlock={({ record }) => renderBlock(record)}
         customRules={[
           renderRule(isBlockquote, ({ node, children, key }) => {
             const props = { node, children, key };
             return <BlockQuote {...props} />;
-          }),
-          renderRule(isCode, ({ node, key }) => {
-            return (
-              <CodeHilight
-                key={key}
-                code={node.code}
-                language={node.language || 'js'}
-                plugins={['line-numbers']}
-              />
-            );
-          }),
-          renderRule(isHeading, ({ node, children, key }) => {
-            return (
-              <h3
-                className="font-semibold uppercase  text-violet-light text-lg mx-auto py-12 px-4"
-                key={key}
-                as={`h${node.level}`}
-              >
-                {children}
-              </h3>
-            );
-          }),
-          renderRule(isList, ({ node, children, key }) => {
-            return (
-              <div key={key}>
-                {node?.style === 'numbered' ? (
-                  <li spacing={0} mb={3}>
-                    {children}
-                  </li>
-                ) : (
-                  <li spacing={0} mb={3}>
-                    {children}
-                  </li>
-                )}
-              </div>
-            );
-          }),
-          renderRule(isListItem, ({ node, children, key }) => {
-            return <ul key={key}>{children}</ul>;
           }),
         ]}
       />
--- pages/[...slugs].js
+++ pages/[...slugs].js
@@ -5,12 +5,15 @@ import Layout from 'components/Layout';
 // import MyStructuredContent from 'components/MyStructuredContent';
 // import SEO from 'components/Seo';
 
-function Page({ page, preview, routes, locale }) {
+function Page({ data, locale }) {
+  console.dir(data);
+  const { pageInfo } = data;
+
   return (
     <Layout>
       <div>
         <ul>
-          {routes?.map((r) => {
+          {/* {routes?.map((r) => {
             const data = r.urls.find((u) => u.locale === locale);
             const { url } = data;
             return (
@@ -22,10 +25,11 @@ function Page({ page, preview, routes, locale }) {
                 </Link>
               </li>
             );
-          })}
+          })} */}
         </ul>
       </div>
-      <pre>{JSON.stringify(page, null, 2)}</pre>
+      <h1>data</h1>
+      {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
     </Layout>
   );
 }
@@ -36,33 +40,9 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params, locale, preview = false }) {
-  console.log('locale', locale);
-  console.log('params', params);
-  
-  // const { slugs } = params;
-  // const slug = slugs.join('/');
-  // console.log(slug);
-
-  // const list = routes.map((r) => {
-  //   const { id, slugs } = r;
-  //   return { id, slug: slugs[locale].join('/') };
-  // });
-  // console.log('list', list);
-
-  // const match = list.find((l) => l.slug === slug);
-  // const pageInfo = routes.find((r) => r.id === match.id);
-  // console.log('pageInfo', pageInfo);
-
-  const { id, type, pageInfo, data } = doQuery(locale, params);
-  console.log(data);
-
+  const data = await doQuery(locale, params);
   return {
-    props: {
-      page: pageInfo ? pageInfo : null,
-      preview,
-      data,
-      locale,
-    },
+    props: { data, locale },
   };
 }
 export default Page;
