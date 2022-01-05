e846b5e8a870de8510799e799ad95d36eb6f04eb
Lorezz <point.point@gmail.com>
2022-01-05 12:12:39 +0100

    refactor: keys to language switcher

--- components/LanguageSwitcher.js
+++ components/LanguageSwitcher.js
@@ -1,4 +1,6 @@
+import { Fragment } from 'react';
 import Link from 'next/link';
+
 import config from 'data/config.json';
 
 function LanguageSwitcher({ locale, alts }) {
@@ -10,12 +12,12 @@ function LanguageSwitcher({ locale, alts }) {
           const isActive = locale === l;
           const link = alts?.find((alt) => alt.locale === l)?.url ?? '';
           return (
-            <>
+            <Fragment key={l}>
               {i > 0 && <span className="text-xs text-gray-600">/</span>}
-              <Link href={`/${link}`} key={`${l}`} locale={l}>
+              <Link href={`/${link}`} locale={l}>
                 <a className={`${isActive ? 'active' : ''}`}>{l}</a>
               </Link>
-            </>
+            </Fragment>
           );
         })}
     </>
