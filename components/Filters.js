72b3433b4d8305129b66312880f873e572035b4b
Lorezz <point.point@gmail.com>
2022-01-05 19:34:38 +0100

    wip filters

--- components/Filters.js
+++ components/Filters.js
@@ -1,18 +1,42 @@
 import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
 import Tabs from 'components/Tabs';
 import ResultsGrid from 'components/ResultsGrid';
 
 function Filters({ locale, list }) {
   const [typeFilter, setTypeFilter] = useState('festival-events');
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
   return (
     <div className="mt-10">
       <div className="mx-10  border-b border-gray-200 flex justify-between align-center  flex-col-reverse md:flex-row">
         <Tabs
-          current={typeFilter}
+          selected={typeFilter}
           handleSelect={(value) => setTypeFilter(value)}
         />
-        <button className="form__button">DATE</button>
+        <div className="mx-10 flex justify-end">
+          <div className="mx-4">FROM</div>
+          <div>
+            <DatePicker
+              selected={startDate}
+              onChange={(date) => setStartDate(date)}
+            />
+          </div>
+          <div className="mx-4">TO</div>
+          <div>
+            <DatePicker
+              selected={endDate}
+              onChange={(date) => setEndDate(date)}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="mt-10">
+        <div className="text-md">TYPE: {typeFilter}</div>
+        <div className="text-md">START: {JSON.stringify(startDate)}</div>
+        <div className="text-md">END: {JSON.stringify(endDate)}</div>
       </div>
       <ResultsGrid list={list} locale={locale} />
     </div>
--- components/Tabs.js
+++ components/Tabs.js
@@ -8,20 +8,33 @@ const tabs = [
   { name: 'Courses', slug: 'courses' },
 ];
 
-export default function Tabs({ current = 'festival-events', handleSelect }) {
+export default function Tabs({ selected, handleSelect }) {
+  const [current, setCurrent] = useState('festival-events');
+  function handleChange(value) {
+    console.log('handleChange', value);
+    if (value && handleSelect) {
+      handleSelect(value);
+    }
+  }
+
+  useEffect(() => {
+    if (selected) {
+      setCurrent(selected);
+    }
+  }, [selected]);
+
   return (
     <div>
       <div className="sm:hidden">
         <label htmlFor="tabs" className="sr-only">
-          Select a tab
+          -
         </label>
-
         <select
           id="tabs"
           name="tabs"
           className="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-black focus:border-black sm:text-sm rounded-md"
           defaultValue={current}
-          onChange={(e) => handleSelect(e.target.value)}
+          onChange={(e) => handleChange(e.target.value)}
         >
           {tabs.map((tab) => (
             <option key={tab.slug}>{tab.name}</option>
@@ -31,21 +44,24 @@ export default function Tabs({ current = 'festival-events', handleSelect }) {
       <div className="hidden sm:block">
         <div>
           <nav className="-mb-px flex space-x-8" aria-label="Tabs">
-            {tabs.map((tab) => (
-              <button
-                onClick={() => handleSelect(tab.slug)}
-                key={tab.slug}
-                className={
-                  (current == tab.slug
-                    ? 'text-primary uppercase'
-                    : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
-                  'whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm')
-                }
-                aria-current={current == tab.slug ? 'page' : undefined}
-              >
-                {tab.name}
-              </button>
-            ))}
+            {tabs.map((tab) => {
+              const { slug, name } = tab;
+              const isActive = slug === current;
+              return (
+                <button
+                  onClick={() => handleChange(slug)}
+                  key={slug}
+                  className={`${
+                    isActive
+                      ? 'text-primary uppercase'
+                      : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
+                  } whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm`}
+                  aria-current={isActive ? 'page' : undefined}
+                >
+                  {name}
+                </button>
+              );
+            })}
           </nav>
         </div>
       </div>
