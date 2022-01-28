92af505dedc7939e8b6c64b6d06dade91455f51f
Lorezz <point.point@gmail.com>
2022-01-28 14:36:26 +0100

    test serverside ty page

--- pages/forms/thankyou.js
+++ pages/forms/thankyou.js
@@ -1,6 +1,5 @@
 import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
-import queryString from 'query-string';
 
 import * as q from 'lib/queries';
 import fetchData from 'lib/api/dato';
@@ -8,54 +7,39 @@ import { doQueryItem, doQueryById } from 'lib/api';
 import Layout from 'components/Layout';
 import Seo from 'components/Seo';
 
-function Page({ data, locale, thankyouMessage }) {
+function Page({ data, locale, thankyouMessage, choiche }) {
   const { site, menu, footer } = data;
-  const [payload, setPayload] = useState(null);
-  const [triggher, setTRiggher] = useState(null);
-  const router = useRouter();
-  const { query } = router;
+  // const router = useRouter();
+  // const { query } = router;
 
-  async function getData({ id, cp }) {
-    if (id && cp) {
-      const data = await doQueryItem(locale, id);
-      const choiche = data?.paymentSettings.find((p) => p.id === cp);
-      const p = { data, choiche };
-      console.log('payload data:', p);
-      setPayload(p);
-    }
-  }
+  // const [choiche, setChoiche] = useState(null)
+  // async function getData({ id, cp }) {
+  //   if (id && cp) {
+  //     const data = await doQueryItem(locale, id);
+  //     const choiche = data?.paymentSettings.find((p) => p.id === cp);
+  //     console.log('choiche data:', p);
+  //     setChoice(choiche);
+  //   }
+  // }
+  // useEffect(() => {
+  //   if (query) {
+  //     console.log('QUERY2:', query);
+  //     getData(query);
+  //   }
+  // }, [query]);
 
-  useEffect(() => {
-    // if (window.location.search.includes('success=true'))
-    const q = queryString.parse(location.search);
-    console.log('QUERY1:', q);
-    getData(q);
-  }, []);
-
-  useEffect(() => {
-    if (query) {
-      console.log('QUERY2:', query);
-      getData(query);
-    }
-  }, [query]);
-
-  // console.log(payload);
   return (
     <Layout footer={footer} menu={menu} locale={locale} hideNewsletter={true}>
       <Seo tags={site.faviconMetaTags} />
-      {triggher}
+
       <div className="p-10 my-10">
         <h1 className="text-lg">{thankyouMessage.titleThanks}</h1>
         <p>{thankyouMessage.textThanks}</p>
-        {!payload && <div>Loading...</div>}
-        {!!payload && payload?.choiche && (
+        {!choiche && <div>Loading...</div>}
+        {choiche && (
           <div className="bg-gray-50 p-4">
-            <a
-              href={payload?.choiche?.paymentLink}
-              target="_blank"
-              rel="noopener"
-            >
-              {`${payload.choiche.description} - ${payload?.choiche?.amount}€`}
+            <a href={choiche?.paymentLink} target="_blank" rel="noopener">
+              {`${choiche?.description} - ${choiche?.amount}€`}
             </a>
           </div>
         )}
@@ -64,13 +48,27 @@ function Page({ data, locale, thankyouMessage }) {
   );
 }
 
-export async function getStaticProps({ params, locale }) {
+export async function getServerSideProps({ locale, query }) {
+  const { id, cp } = query;
+  console.log('WUERY', query);
   const data = await doQueryById(locale);
   const ty = await fetchData(q.extra_content, { locale });
+  const payload = await doQueryItem(locale, id);
+
+  const choiche = payload.paymentSettings?.find((p) => p.id === query.cp);
   const thankyouMessage = ty.extraContent;
-  console.log('thankyouMessage', thankyouMessage);
   return {
-    props: { data, locale, thankyouMessage },
+    props: { data, locale, thankyouMessage, payload, query, choiche },
   };
 }
+
+// export async function getStaticProps({ params, locale }) {
+//   const data = await doQueryById(locale);
+//   const ty = await fetchData(q.extra_content, { locale });
+//   const thankyouMessage = ty.extraContent;
+//   console.log('thankyouMessage', thankyouMessage);
+//   return {
+//     props: { data, locale, thankyouMessage },
+//   };
+// }
 export default Page;
