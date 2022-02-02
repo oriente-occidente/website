31c4c44f4bac8346a8776591ae0d93cc4078f8c1
Lorezz <point.point@gmail.com>
2022-02-02 15:55:50 +0100

    without dynamic import

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 import { Fragment } from 'react';
-import dynamic from 'next/dynamic';
 
 import translate from 'lib/locales';
 import * as queries from 'lib/queries';
@@ -8,33 +7,31 @@ import fetchDato from 'lib/api/dato';
 import { resolveLinkById } from 'lib/utils';
 
 import Layout from 'components/Layout';
-// import Seo from 'components/Seo';
-// import HeroSlider from 'components/hero/HeroSlider';
-// import GalleryPreview from 'components/galleries/GalleryPreview';
-// import GalleryHome from 'components/galleries/GalleryHome';
+import Seo from 'components/Seo';
+import HeroSlider from 'components/hero/HeroSlider';
+import GalleryPreview from 'components/galleries/GalleryPreview';
+import GalleryHome from 'components/galleries/GalleryHome';
 
-let Seo = dynamic(() => import('components/Seo'), {
-  loading: () => <div>...</div>,
-  ssr: false,
-});
-
-let GalleryHome = dynamic(() => import('components/galleries/GalleryHome'), {
-  loading: () => <div>...</div>,
-  ssr: false,
-});
-
-let GalleryPreview = dynamic(
-  () => import('components/galleries/GalleryPreview'),
-  {
-    loading: () => <div />,
-    ssr: false,
-  }
-);
-
-let HeroSlider = dynamic(() => import('components/hero/HeroSlider'), {
-  loading: () => <div />,
-  ssr: false,
-});
+// import dynamic from 'next/dynamic';
+// let Seo = dynamic(() => import('components/Seo'), {
+//   loading: () => <div>...</div>,
+//   ssr: false,
+// });
+// let GalleryHome = dynamic(() => import('components/galleries/GalleryHome'), {
+//   loading: () => <div>...</div>,
+//   ssr: false,
+// });
+// let GalleryPreview = dynamic(
+//   () => import('components/galleries/GalleryPreview'),
+//   {
+//     loading: () => <div />,
+//     ssr: false,
+//   }
+// );
+// let HeroSlider = dynamic(() => import('components/hero/HeroSlider'), {
+//   loading: () => <div />,
+//   ssr: false,
+// });
 
 function Home({ data, locale, home }) {
   const { homeSlideshow, homeSections, seo } = home;
