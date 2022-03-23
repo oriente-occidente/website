f9d6bc1a3cddaff9b14277cef90cc5ab7e8ce4fa
Guido Squillace <g.squillace@cantierecreativo.net>
2022-03-23 10:05:11 +0100

    Remove active lang

--- components/LanguageSwitcherLanding.js
+++ components/LanguageSwitcherLanding.js
@@ -17,16 +17,16 @@ function LanguageSwitcherLanding({ locale, alts }) {
           }
           return (
             <Fragment key={l}>
-              {i > 0 && (
+              {/* {i > 0 && (
                 <span className="text-xs text-gray-600 hover:text-red lg:block">
                   /
                 </span>
-              )}
+              )} */}
               <Link href={`/landing/${link}`} locale={l}>
                 <a
                   className={`${
-                    isActive ? "font-semibold" : ""
-                  } hover:text-red lg:block`}
+                    isActive ? "hidden" : ""
+                  } font-semibold hover:text-red`}
                 >
                   {l}
                 </a>
