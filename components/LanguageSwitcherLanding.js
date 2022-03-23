e89788445b0ae3a2874c86b2e9687cfe540627dd
Guido Squillace <g.squillace@cantierecreativo.net>
2022-03-23 09:58:15 +0100

    Resum link switcher

--- components/LanguageSwitcherLanding.js
+++ components/LanguageSwitcherLanding.js
@@ -6,37 +6,29 @@ import translate from "lib/locales";
 
 function LanguageSwitcherLanding({ locale, alts }) {
   const { locales } = config.site;
-  const itLanguage = {
-    show: "en",
-    href: "https://www.orienteoccidente.it/en/landing/presenti-accessibili-en",
-  };
-  const enLanguage = {
-    show: "it",
-    href: "https://www.orienteoccidente.it/landing/presenti-accessibili",
-  };
   return (
     <>
       {locales &&
         locales.map((l, i) => {
           const isActive = locale === l;
           const link = alts?.find((alt) => alt.locale === l)?.value ?? "";
+          {
+            console.log("alts:", alts);
+          }
           return (
             <Fragment key={l}>
-              {/* {i > 0 && (
+              {i > 0 && (
                 <span className="text-xs text-gray-600 hover:text-red lg:block">
                   /
                 </span>
-              )} */}
-              <Link
-                href={`${l == "it" ? itLanguage.href : enLanguage.href}`}
-                // locale={l}
-              >
+              )}
+              <Link href={`/landing/${link}`} locale={l}>
                 <a
                   className={`${
-                    isActive ? "font-semibold" : "hidden"
-                  } hover:text-red`}
+                    isActive ? "font-semibold" : ""
+                  } hover:text-red lg:block`}
                 >
-                  {`${l == "it" ? itLanguage.show : enLanguage.show}`}
+                  {l}
                 </a>
               </Link>
             </Fragment>
