dd36ccdd867bf1b5c76e1279995991df59595fcd
Lorezz <point.point@gmail.com>
2022-04-20 16:32:33 +0200

    test payment success

--- pages/forms/payment.js
+++ pages/forms/payment.js
@@ -6,20 +6,25 @@ import Layout from 'components/Layout';
 import Seo from 'components/Seo';
 import t from 'lib/locales';
 
-function Page({ data, locale,   }) {
+function Page({ data, locale }) {
   const { site, menu, footer } = data;
-const [nexi,setNexi] = useState()
-useEffect(() => {
-    const paymentInfo = await fetch('/api/xpay', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-    },
-    body: JSON.stringify({ code: 123, amount: 15 }),
-  }).then((r) => r.json());
-  setNexi(paymentInfo);
-},[])
+  const [nexi, setNexi] = useState();
+  useEffect(() => {
+    async function getData() {
+      const paymentInfo = await fetch('/api/xpay', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        body: JSON.stringify({ code: 123, amount: 15 }),
+      }).then((r) => r.json());
+
+      console.log('paymentInfo', paymentInfo);
+      setNexi(paymentInfo);
+    }
+    getData();
+  }, []);
 
   return (
     <Layout footer={footer} menu={menu} locale={locale} hideNewsletter={true}>
@@ -27,21 +32,21 @@ useEffect(() => {
       <div className="my-10 p-10">
         <h1 className="h1 mt-10">{t('payment', locale)}</h1>
         {nexi && (
-        <div className={`mb-10 rounded-lg border p-4 `}>
-          <form name="nexi" method="POST" action={nexi.url}>
-            {Object.keys(nexi.params).map((k) => (
-              <input type="hidden" key={k} name={k} value={nexi.params[k]} />
-            ))}
-            <div>
-              <button
-                type="submit"
-                className="mt-10 min-w-full border border-gray-700 bg-white py-2 px-4 font-semibold text-gray-700 hover:border-black hover:text-black"
-              >
-                {t('send', locale)}
-              </button>
-            </div>
-          </form>
-        </div>
+          <div className={`mb-10 rounded-lg border p-4 `}>
+            <form name="nexi" method="POST" action={nexi.url}>
+              {Object.keys(nexi.params).map((k) => (
+                <input type="hidden" key={k} name={k} value={nexi.params[k]} />
+              ))}
+              <div>
+                <button
+                  type="submit"
+                  className="mt-10 min-w-full border border-gray-700 bg-white py-2 px-4 font-semibold text-gray-700 hover:border-black hover:text-black"
+                >
+                  {t('send', locale)}
+                </button>
+              </div>
+            </form>
+          </div>
         )}
       </div>
     </Layout>
