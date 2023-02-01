522ab51ede47092e661e5f076987c990f832a7d5
Alberto De Stefani <orionlight231@gmail.com>
2023-02-01 17:49:53 +0100

    sitemap function

---
+++ scripts/getSitemap.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+let HOST = process.env.HOST || "";
+const NEWLINE = "";
+
+function getRoute(str) {
+  return `
+  <url>
+    <loc>${HOST}${str}</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>`;
+}
+
+function generateSitemap() {
+  const rawdata = fs.readFileSync("data/urls.json");
+  const urls = JSON.parse(rawdata);
+  const start = Date.now();
+  const sitemap = `
+    <\?xml version="1.0" encoding="UTF-8"\?>
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
+        ${urls.map((p) => getRoute(p)).join(NEWLINE)}
+    </urlset>`;
+  fs.writeFileSync(`data/sitemap.xml`, sitemap.trim());
+  const elapsed = (Date.now() - start) / 1000;
+  console.log("DONE GENERATE SITEMAP in", elapsed);
+}
+
+module.exports = generateSitemap;
--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,22 @@
-const nodeEnv = process.env.NODE_ENV || 'develop';
-if (nodeEnv === 'develop') {
-  require('dotenv').config({ path: '.env.local' });
+const nodeEnv = process.env.NODE_ENV || "develop";
+if (nodeEnv === "develop") {
+  require("dotenv").config({ path: ".env.local" });
 }
 
-const getConfig = require('./getConfig');
-const getRoutes = require('./getRoutes');
+const getConfig = require("./getConfig");
+const getRoutes = require("./getRoutes");
+const getSitemap = require("./getSitemap");
 
 (async () => {
   try {
     const start = Date.now();
     await getRoutes();
     await getConfig();
-
-    // generatesitemap
+    getSitemap();
 
     const elapsed = (Date.now() - start) / 1000;
     console.log(`script done in ${elapsed.toFixed(2)} sec`);
   } catch (error) {
-    console.error('ERROR', error);
+    console.error("ERROR", error);
   }
 })();
