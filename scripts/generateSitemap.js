e91ecf238a6e71966200934ddc8e4a3f5521b8c6
Lorezz <point.point@gmail.com>
2023-02-03 15:06:19 +0100

    :minus: removed next-sitemap

--- scripts/generateSitemap.js
+++ scripts/generateSitemap.js
@@ -1,6 +1,6 @@
-const fs = require("fs");
-let HOST = process.env.HOST || "";
-const NEWLINE = "";
+const fs = require('fs');
+let HOST = process.env.HOST || '';
+const NEWLINE = '';
 
 function getRoute(str) {
   return `
@@ -13,17 +13,17 @@ function getRoute(str) {
 }
 
 function generateSitemap() {
-  const rawdata = fs.readFileSync("data/urls.json");
+  const rawdata = fs.readFileSync('data/urls.json');
   const urls = JSON.parse(rawdata);
   const start = Date.now();
   const sitemap = `
     <\?xml version="1.0" encoding="UTF-8"\?>
       <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
-        ${urls.map((p) => getRoute(p)).join(NEWLINE)}
+        ${['', ...urls].map((p) => getRoute(p)).join(NEWLINE)}
     </urlset>`;
-  fs.writeFileSync(`data/sitemap.xml`, sitemap.trim());
+  fs.writeFileSync(`public/sitemap.xml`, sitemap.trim());
   const elapsed = (Date.now() - start) / 1000;
-  console.log("DONE GENERATE SITEMAP in", elapsed);
+  console.log('DONE GENERATE SITEMAP in', elapsed);
 }
 
 module.exports = generateSitemap;
