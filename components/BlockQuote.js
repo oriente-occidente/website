2677854ac021fd1ac128c7da5e9315df0b3a47d7
Alessandro Donnini <info@alessandrodonnini.com>
2023-03-15 16:48:14 +0100

    Update font weight

--- components/BlockQuote.js
+++ components/BlockQuote.js
@@ -1,7 +1,7 @@
 const BlockQuote = ({ node, children }) => {
   return (
     <div className="my-2 max-w-7xl border-l-2 border-black px-4 sm:px-6 lg:my-10 lg:px-8 2xl:my-16 ">
-      <div className="text-secondary mt-1 text-xl font-light lg:text-3xl">
+      <div className="text-secondary mt-1 text-xl lg:text-3xl">
         <i>{children}</i>
       </div>
       {node.attribution && (
