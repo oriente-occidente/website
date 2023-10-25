fe4a4551b3702f51fa4a40ccf8e918f288c60705
Alberto De Stefani <orionlight231@gmail.com>
2023-10-25 17:04:01 +0200

    query program

--- src/app/festival/[slug]/page.tsx
+++ src/app/festival/[slug]/page.tsx
@@ -1,8 +1,30 @@
 import { draftMode } from "next/headers";
+import { notFound } from "next/navigation";
 import type { BasicSlugPageProps } from "@/types";
+import queryDatoCMS from "@/lib/fetchDato";
+import { PageDocument, ProgramDocument, SiteLocale } from "@/graphql/generated";
+
 const locale = "it";
-export default function Page({ params }: BasicSlugPageProps) {
+export default async function Page({ params }: BasicSlugPageProps) {
+  const { slug } = params;
   const { isEnabled: preview } = draftMode();
+  const { isEnabled } = draftMode();
+  const siteLocale = locale as SiteLocale;
+  const data = await queryDatoCMS(PageDocument, { locale: siteLocale, slug }, isEnabled);
+
+  if (!data.page) notFound();
+  let list;
+  if (data.page.isIndex) {
+    const { festivalEvents, otherEvents, workshops } = await queryDatoCMS(
+      ProgramDocument,
+      { locale: siteLocale },
+      isEnabled
+    );
+    list = [...festivalEvents, ...otherEvents, ...workshops];
+  }
+
+  // console.log("LIST", list);
+
   return (
     <div>
       <div className="text-xl">
