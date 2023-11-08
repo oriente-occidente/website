e0b778e3ec4ec59c5b736d08ea8f33334b00290e
Alessandro Donnini <info@alessandrodonnini.com>
2023-11-08 15:46:03 +0100

    Add hero in news detail

--- src/app/(base)/news/[slug]/page.tsx
+++ src/app/(base)/news/[slug]/page.tsx
@@ -3,6 +3,7 @@ import type { BasicSlugPageProps } from "@/types";
 import { NewsDocument, SiteLocale } from "@/graphql/generated";
 import queryDatoCMS from "@/lib/fetchDato";
 import MainContent from "@/components/contents/MainContent";
+import HeroIndex from "@/components/hero/HeroIndex";
 
 const locale = "it";
 export default async function Page({ params }: BasicSlugPageProps) {
@@ -10,9 +11,19 @@ export default async function Page({ params }: BasicSlugPageProps) {
   const { isEnabled: preview } = draftMode();
   const { isEnabled } = draftMode();
   const siteLocale = locale as SiteLocale;
-  const data = await queryDatoCMS(NewsDocument, { locale: siteLocale, slug }, isEnabled);
-  const { layoutHero, titleHero, descriptionHero, imageHero, slideshowHero, startDate } =
-    data?.news || {};
+  const data = await queryDatoCMS(
+    NewsDocument,
+    { locale: siteLocale, slug },
+    isEnabled
+  );
+  const {
+    layoutHero,
+    titleHero,
+    descriptionHero,
+    imageHero,
+    slideshowHero,
+    startDate,
+  } = data?.news || {};
 
   // console.log('newsDate', newsDate);
   const heroData: any = {
@@ -25,6 +36,7 @@ export default async function Page({ params }: BasicSlugPageProps) {
 
   return (
     <div>
+      <HeroIndex data={heroData} locale={locale} />
       <MainContent locale={locale} data={data.news?.content} />
     </div>
   );
