07d949b5d7aa5dca341b5ad581bdf8e96822f361
Lorezz <point.point@gmail.com>
2023-11-14 12:24:23 +0100

    wip menu

--- src/app/(base)/studio/compagnie/[slug]/page.tsx
+++ src/app/(base)/studio/compagnie/[slug]/page.tsx
@@ -1,11 +1,15 @@
-import { draftMode } from "next/headers";
 import type { BasicSlugPageProps } from "@/types";
 const locale = "it";
+
 export default function Page({ params }: BasicSlugPageProps) {
+  const list = ["eventi", "linguaggi", "workshop", "artisti", "compagnie"];
+  const idx = Math.ceil(Math.random() * list.length - 1);
+  const name = `card-title-${list[idx]}`;
+  const cn = ["text-xl", name];
   return (
-    <div>
-      <div className="text-xl">
-        My slug page: {params.slug} - {locale}
+    <div className="bg-black">
+      <div className={cn.join(" ")}>
+        My slug page: {params.slug} - {locale} - {name}
       </div>
       <div>LOCALE:{locale}</div>
     </div>
