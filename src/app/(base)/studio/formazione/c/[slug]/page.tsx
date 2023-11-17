ac1eede43a7e265e2237535f2b87d28e79dd6927
Alberto De Stefani <orionlight231@gmail.com>
2023-11-17 15:15:26 +0100

    handle all category

--- src/app/(base)/studio/formazione/c/[slug]/page.tsx
+++ src/app/(base)/studio/formazione/c/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import { draftMode } from "next/headers";
 import queryDatoCMS from "@/lib/fetchDato";
-import { SiteLocale, WorkshopByCategoryQueryDocument } from "@/graphql/generated";
+import { SiteLocale, AllWorkshopsByCategoryQueryDocument } from "@/graphql/generated";
 import type { BasicSlugPageProps } from "@/types";
 import WorkshopByCategory from "@/components/WorkshopByCategory";
 
@@ -9,13 +9,30 @@ export default async function Page({ params }: BasicSlugPageProps) {
   const { slug } = params;
   const { isEnabled } = draftMode();
   const siteLocale = locale as SiteLocale;
+
   const data = await queryDatoCMS(
-    WorkshopByCategoryQueryDocument,
-    { locale: siteLocale, slug: slug != "all" ? slug : null },
+    AllWorkshopsByCategoryQueryDocument,
+    { locale: siteLocale, slug: slug != "all" ? slug : undefined },
     isEnabled
   );
+
+  let workshopCat: string = "";
+  let list: any = [];
+
+  if (data.allWorkshopCategories && data.allWorkshopCategories.length > 0) {
+    if (data.allWorkshopCategories.length == 1) {
+      workshopCat = data.allWorkshopCategories[0].slug;
+      list = data.allWorkshopCategories[0]._allReferencingWorkshops;
+    } else {
+      workshopCat = "all";
+      list = data.allWorkshopCategories
+        .map(({ _allReferencingWorkshops }) => _allReferencingWorkshops)
+        .filter((a) => a.length > 0);
+    }
+  }
+
   const heroData: any = {
-    titleHero: data?.workshopCategory?.title || "",
+    titleHero: "Workshop",
   };
   // const pageData: any = {
   //   hero: heroData,
@@ -24,8 +41,8 @@ export default async function Page({ params }: BasicSlugPageProps) {
 
   return (
     <WorkshopByCategory
-      list={data.workshopCategory?._allReferencingWorkshops || []}
-      workshopCat={slug}
+      list={list.flat()}
+      workshopCat={workshopCat}
       locale={locale}
       heroData={heroData}
     />
