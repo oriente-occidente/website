50028bea525ef469d47bf5f433710f82e537ba99
Lorezz <point.point@gmail.com>
2023-11-27 16:43:35 +0100

    test build

--- src/app/(base)/archivio/festival/page.tsx
+++ src/app/(base)/archivio/festival/page.tsx
@@ -1,7 +1,6 @@
 import { draftMode } from "next/headers";
-import { ProgramDocument, SiteLocale } from "@/graphql/generated";
+import { FestivalIndexDocument, SiteLocale } from "@/graphql/generated";
 import queryDatoCMS from "@/lib/fetchDato";
-import NewsCard from "@/components/cards/NewsCard";
 import CategoryCard from "@/components/cards/CategoryCard";
 import { Fragment } from "react";
 
@@ -11,14 +10,12 @@ export default async function Page() {
   const { isEnabled } = draftMode();
   const siteLocale = locale as SiteLocale;
   const data = await queryDatoCMS(
-    ProgramDocument,
+    FestivalIndexDocument,
     { locale: siteLocale },
     isEnabled
   );
 
   const { festivalEvents }: any = data;
-
-  console.log("FESTIVAL EVENTS", festivalEvents);
 
   return (
     <div className="container lg:grid lg:grid-cols-2 lg:gap-6">
