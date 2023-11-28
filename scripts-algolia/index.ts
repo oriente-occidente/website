17ae611310a21934518aa916a2e45975082f3da4
Lorezz <point.point@gmail.com>
2023-11-28 18:07:48 +0100

    wip index

--- scripts-algolia/index.ts
+++ scripts-algolia/index.ts
@@ -1,15 +1,14 @@
-// import videoSearch from "./video-search";
+import search from "./sampleSearch";
 import { listIndexes } from "./algolia-utils";
 
-const LANGS = ["it", "en"];
 (async () => {
   const start = Date.now();
   const indexes = await listIndexes();
   console.info("INDEXES", indexes);
 
   // VIDEO SEARCH
-  // await videoSearch(LANGS[0], indexes);
-  // await videoSearch(LANGS[1], indexes);
+  await search("it", indexes);
+  await search("en", indexes);
 
   const elapsed = Date.now() - start;
   console.info("ELAPSED", elapsed / 1000, "seconds");
--- scripts-algolia/sampleSearch.ts
+++ scripts-algolia/sampleSearch.ts
@@ -1,118 +1,72 @@
 import { getCollections, formatStructuredText } from "./dato-utils";
 import { sendIndex } from "./algolia-utils";
 
-const query = `query allArtworksQuery($first: IntType, $skip: IntType, $locale: SiteLocale) {
-    allArtworks(locale: $locale, first: $first, skip: $skip, orderBy: position_ASC) {
-      id
-      apiKey: _modelApiKey
-      slug(locale: $locale)
-      title(locale: $locale)
-      abstract
-      position
-      author
-      featured
-      previewImage {
-        id
-        url(imgixParams: {ar: "3:4",  fit: crop, w: "640", auto: format})
-      }
-      blocks(locale: $locale) {
-        __typename
-        ... on TextBlockRecord {
-          id
-          content {
-            value
-          }
-        }
-      }
-      artworkCollection {
-        id
-        title(locale: $locale)
-        slug(locale: $locale)
-        museum {
-          id
-          title(locale: $locale)
-          slug(locale: $locale)
-        }
-      }
-      temporaryLocation {
-        id
-        title(locale: $locale)
-        slug(locale: $locale)
-        project {
-          id
-          title(locale: $locale)
-          slug(locale: $locale)
-        }
-      }
-      descriptiveTags {
-        id
-        title(locale: $locale)
-        slug
-      }
+const query = `query associatedArtists($locale: SiteLocale, $first: IntType, $skip: IntType) {
+   allArtists(
+    locale: $locale
+    filter: {slug: {neq: null}, associatedArtist: {exists: true}}
+    orderBy: _firstPublishedAt_DESC
+    first: $first
+    skip: $skip
+  ) {
+    id
+    title
+    slug
+    apiKey: _modelApiKey
+    startDate: _firstPublishedAt
+    image: imageHero {
+      url(imgixParams: {auto: [format, compress], ar: "5:4", fit: crop})
     }
-  }`;
-  
+    content {
+      value
+    }
+    years: associatedArtist {
+      year
+    }
+    countries {
+      name
+    }
+  }
+}`;
+
 async function formatItem(item: any) {
-  let {
-    id,
-    slug,
-    apiKey,
-    position,
-    title,
-    abstract,
-    previewImage,
-    blocks,
-    descriptiveTags,
-    ...rest
-  } = item;
-  let contents = await Promise.all(
-    blocks.map((block: any) =>
-      block.content ? formatStructuredText(block.content) : Promise.resolve("")
-    )
-  );
-  const content: string | null = contents?.join(" ") || null;
+  let { id, slug, apiKey, title, image, years, ...rest } = item;
+  // let contents = await Promise.all(
+  //   blocks.map((block: any) =>
+  //     block.content ? formatStructuredText(block.content) : Promise.resolve("")
+  //   )
+  // );
+  // const content: string | null = contents?.join(" ") || null;
+  const content = await formatStructuredText(rest.content);
   const objectID = id;
-  const tags = descriptiveTags?.map((tag: any) => tag.title) ?? [];
   return {
     objectID,
     slug,
     title: title?.trim() ?? "",
-    image: previewImage?.url,
+    image: image?.url,
     content,
-    tags,
     ...rest,
+    years: years.map((i: any) => i.year),
   };
 }
 
-export default async function artworkSearch(locale: string, indexes: string[]) {
-  console.info("ARTWORK", locale);
+const NAME = "artists";
+export default async function search(locale: string, indexes: string[]) {
+  console.info(NAME, locale);
 
-  const artworks: any[] = await getCollections(
-    query,
-    { locale },
-    "allArtworks"
-  );
-  console.info("TOTAL", artworks.length);
+  const items: any[] = await getCollections(query, { locale }, "items");
+  console.info("TOTAL", items.length);
   const data = [];
-  for (let i = 0; i < artworks.length; i++) {
-    const item: any = artworks[i];
+  for (let i = 0; i < items.length; i++) {
+    const item: any = items[i];
     const formatted = await formatItem(item);
     data.push(formatted);
   }
 
-  const name = `artworks`;
-  const indexName = `${name}_${locale}`;
-  const searchableAttributes = [
-    "author",
-    "title",
-    "slug",
-    "abstract",
-    "content",
-    "tags",
-    "categories",
-  ];
-  const attributesForFaceting = ["featured", "searchable(tags)"];
-  const customRanking: string[] = ["asc(position)"];
+  const indexName = `${NAME}_${locale}`;
+  const searchableAttributes = ["title", "slug", "content"];
+  const attributesForFaceting = ["searchable(years)"];
+  const customRanking: string[] = [];
 
   let replace = false;
   if (indexes.includes(indexName)) {
