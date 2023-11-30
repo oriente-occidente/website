3be70029ab14fb806ee772bcdd01c136e73ab8f5
Lorezz <point.point@gmail.com>
2023-11-30 12:40:49 +0100

    wip search

---
+++ src/components/templates/SearchTemplate.tsx
@@ -0,0 +1,18 @@
+import { GenericSearchProps } from "@/types";
+
+export default function SearchTemplate({
+  data,
+  locale,
+  children,
+}: GenericSearchProps) {
+  const { title, description } = data;
+  return (
+    <div className="container py-10">
+      <h1 className="text-2xl font-bold">{title}</h1>
+      <div className="w-full border-solid border-b-2 border-gray-100 py-10">
+        {description}
+      </div>
+      <div className="mt-10">{children}</div>
+    </div>
+  );
+}
