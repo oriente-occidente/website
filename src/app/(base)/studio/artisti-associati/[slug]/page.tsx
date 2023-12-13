f3f07a8d02f7e5ead6fe73df5b471513a9c3d477
Lorezz <point.point@gmail.com>
2023-12-13 15:19:03 +0100

    comapny and seo on artists

--- src/app/(base)/studio/artisti-associati/[slug]/page.tsx
+++ src/app/(base)/studio/artisti-associati/[slug]/page.tsx
@@ -1,15 +1,30 @@
 import { draftMode } from "next/headers";
 import type { BasicSlugPageProps } from "@/types";
 import { ArtistDocument, SiteLocale } from "@/graphql/generated";
-import queryDatoCMS from "@/lib/fetchDato";
 import PageTemplate from "@/components/templates/PageTemplate";
+import fetchDato from "@/lib/fetchDato";
+import getSeoMeta from "@/lib/seoUtils";
 
 const locale = "it";
+
+export async function generateMetadata({ params }: BasicSlugPageProps) {
+  const { slug } = params;
+  const siteLocale = locale as SiteLocale;
+  const data = await fetchDato(
+    ArtistDocument,
+    { locale: siteLocale, slug },
+    false
+  );
+  const page: any = data?.artist || null;
+  const meta = getSeoMeta(page);
+  return meta;
+}
+
 export default async function Page({ params }: BasicSlugPageProps) {
   const { slug } = params;
   const { isEnabled } = draftMode();
   const siteLocale = locale as SiteLocale;
-  const data = await queryDatoCMS(
+  const data = await fetchDato(
     ArtistDocument,
     { locale: siteLocale, slug },
     isEnabled
--- src/app/(base)/studio/compagnie/[slug]/page.tsx
+++ src/app/(base)/studio/compagnie/[slug]/page.tsx
@@ -1,20 +1,35 @@
 import { draftMode } from "next/headers";
 import type { BasicSlugPageProps } from "@/types";
-import { ArtistDocument, SiteLocale } from "@/graphql/generated";
-import queryDatoCMS from "@/lib/fetchDato";
+import { CompanyDocument, SiteLocale } from "@/graphql/generated";
 import PageTemplate from "@/components/templates/PageTemplate";
+import getSeoMeta from "@/lib/seoUtils";
+import fetchDato from "@/lib/fetchDato";
 
 const locale = "it";
+
+export async function generateMetadata({ params }: BasicSlugPageProps) {
+  const { slug } = params;
+  const siteLocale = locale as SiteLocale;
+  const data = await fetchDato(
+    CompanyDocument,
+    { locale: siteLocale, slug },
+    false
+  );
+  const page: any = data?.company || null;
+  const meta = getSeoMeta(page);
+  return meta;
+}
+
 export default async function Page({ params }: BasicSlugPageProps) {
   const { slug } = params;
   const { isEnabled } = draftMode();
   const siteLocale = locale as SiteLocale;
-  const data = await queryDatoCMS(
-    ArtistDocument,
+  const data = await fetchDato(
+    CompanyDocument,
     { locale: siteLocale, slug },
     isEnabled
   );
-  const page: any = data.artist;
+  const page: any = data?.company;
   const heroData: any = {
     layoutHero: page?.layoutHero,
     titleHero: page?.titleHero,
