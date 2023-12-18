5c4c626a491b1e232b7b5ed4efe8c59b54ce184e
Alessandro Donnini <info@alessandrodonnini.com>
2023-12-18 14:59:55 +0100

    Add video index

--- src/app/(base)/video/page.tsx
+++ src/app/(base)/video/page.tsx
@@ -1,11 +1,68 @@
 import { draftMode } from "next/headers";
+import { notFound } from "next/navigation";
+
+import type { BasicSlugPageProps } from "@/types";
+import getSeoMeta from "@/lib/seoUtils";
+import {
+  VideosIndexDocument,
+  AllVideosWebsiteDocument,
+  SiteLocale,
+} from "@/graphql/generated";
+import fetchDato from "@/lib/fetchDato";
+import HeroEmpty from "@/components/hero/HeroEmpty";
+import MediaCard from "@/components/cards/MediaCard";
+
 const locale = "it";
-export default function Page() {
-  //
+
+// export async function generateMetadata({ params }: BasicSlugPageProps) {
+//   const { slug } = params;
+//   const siteLocale = locale as SiteLocale;
+//   const data = await fetchDato(
+//     VideosIndexDocument,
+//     { locale: siteLocale, slug },
+//     false
+//   );
+//   const page: any = data?.page || null;
+//   const meta = getSeoMeta(page);
+//   return meta;
+// }
+
+export default async function Page({ params }: BasicSlugPageProps) {
+  const { slug } = params;
+
+  const { isEnabled } = draftMode();
+  const siteLocale = locale as SiteLocale;
+  const { videosIndex } = await fetchDato(
+    VideosIndexDocument,
+    { locale: siteLocale, slug },
+    isEnabled
+  );
+
+  const { allMediaVideos } = await fetchDato(
+    AllVideosWebsiteDocument,
+    { locale: siteLocale },
+    isEnabled
+  );
+
+  if (!videosIndex) notFound();
+  const heroData = {
+    titleHero: videosIndex.title,
+    descriptionHero: videosIndex.description,
+  };
+
   return (
     <div>
-      <div className="text-xl">My index page</div>
-      <div>LOCALE:{locale}</div>
+      <HeroEmpty data={heroData} locale={locale} />
+      <div className="container my-5 grid sm:grid-cols-2 lg:grid-cols-3 gap-x-3 sm:gap-x-5 gap-y-8">
+        {allMediaVideos.map((v) => {
+          const data = { ...v, contentType: "audio", image: v.image?.url };
+          return (
+            <div key={v.id}>
+              <MediaCard data={data} locale={locale} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
