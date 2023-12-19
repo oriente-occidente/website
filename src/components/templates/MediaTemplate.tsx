0df6952403b50b4e67b367436f0524bb40d0b9ef
Alessandro Donnini <info@alessandrodonnini.com>
2023-12-19 10:57:52 +0100

    Update media template

--- src/components/templates/MediaTemplate.tsx
+++ src/components/templates/MediaTemplate.tsx
@@ -9,6 +9,8 @@ import MediaVideo from "@/components/MediaVideo";
 import Breadcrumbs from "@/components/Breadcrumbs";
 
 export default function MediaTemplate({ data, locale }: any) {
+  if (!data?._modelApiKey) return;
+
   const { _modelApiKey } = data;
 
   const renderMedia = (apikey: string) => {
@@ -29,9 +31,11 @@ export default function MediaTemplate({ data, locale }: any) {
                 locale={locale}
               />
             </div>
-            <div className="pb-12">
-              <MediaDocumentImage data={data} locale={locale} />
-            </div>
+            {data.image && (
+              <div className="pb-12">
+                <MediaDocumentImage data={data} locale={locale} />
+              </div>
+            )}
             <div className="pb-12 border-b">
               <MediaDownload attachment={data.attachment} locale={locale} />
             </div>
@@ -73,61 +77,13 @@ export default function MediaTemplate({ data, locale }: any) {
   };
 
   return (
-    <div className="mx-auto md:container md:px-0 lg:px-24 xl:px-64">
+    <div className="mx-auto md:container">
       <Breadcrumbs data={data} locale={locale as any} background={null} />
-      {/* {_modelApiKey == "media_photo" && (
-        <div className="border-b pt-12 pb-6 md:pt-16 md:pb-12 lg:pt-24">
-          <MediaPhotoImage photo={data} locale={locale} />
+      <div className=" md:px-0 lg:px-24 xl:px-64">
+        {renderMedia(_modelApiKey)}
+        <div className="py-12">
+          <MediaLinks data={data} locale={locale} />
         </div>
-      )}
-      {_modelApiKey == "media_document" && (
-        <>
-          <div className="pt-12 lg:pt-24 pb-12">
-            <MediaTitle
-              _modelApyKey={data._modelApiKey}
-              data={data}
-              locale={locale}
-            />
-          </div>
-          <div className="pb-12">
-            <MediaDocumentImage data={data} locale={locale} />
-          </div>
-          <div className="pb-12 border-b">
-            <MediaDownload attachment={data.attachment} locale={locale} />
-          </div>
-        </>
-      )}{" "}
-      {_modelApiKey == "media_audio" && (
-        <>
-          <div className="pt-12 lg:pt-24 pb-12">
-            <MediaTitle
-              _modelApyKey={data._modelApiKey}
-              data={data}
-              locale={locale}
-            />
-          </div>
-          <div className="pb-12 border-b">
-            <MediaAudioPlayer data={data} locale={locale} />
-          </div>
-        </>
-      )}
-      {_modelApiKey == "media_video" && (
-        <>
-          <div className="pt-12 lg:pt-24 pb-12">
-            <MediaTitle
-              _modelApyKey={data._modelApiKey}
-              data={data}
-              locale={locale}
-            />
-          </div>
-          <div className="grid grid-cols-1 justify-center pb-6 border-b">
-            <MediaVideo data={data} locale={locale} />
-          </div>
-        </>
-      )} */}
-      {renderMedia(_modelApiKey)}
-      <div className="py-12">
-        <MediaLinks data={data} locale={locale} />
       </div>
     </div>
   );
