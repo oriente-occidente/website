ade379b6afd0d56493d822d2ed78dd9e7b970155
Alessandro Donnini <info@alessandrodonnini.com>
2023-12-19 10:58:03 +0100

    Fix breadcrumbs

--- src/components/Breadcrumbs.js
+++ src/components/Breadcrumbs.js
@@ -3,9 +3,9 @@ import Link from "next/link";
 import resolveLink from "@/lib/resolveLink";
 
 export default function Breadcrumbs({ data, locale, background }) {
-  const link = resolveLink({ locale, ...data });
+  const d = { ...data, slug: data.slug ? data.slug : data.id }
+  const link = resolveLink({ locale, ...d });
   const paths = link.split("/").filter((p) => p);
-
   return (
     <nav
       className={`hidden md:block bg-${background} border-y border-y-gray py-2 xl:py-5`}
@@ -14,17 +14,23 @@ export default function Breadcrumbs({ data, locale, background }) {
       <div className="container flex">
         <ol role="list" className="flex items-center space-x-4">
           {["Home", ...paths].map((p, i) => {
+            const total = paths.length
             return (
               <li key={p} className="group">
                 <div className="flex items-center">
-                  <Link
-                    href={i === 0 ? "/" : link}
-                    locale={locale}
-                    className="text-[12px] uppercase hover:text-red"
-                    aria-current={i === paths.length ? "page" : undefined}
-                  >
-                    {p}
-                  </Link>
+                  {i < total &&
+                    <Link
+                      href={i === 0 ? "/" : link}
+                      locale={locale}
+                      className="text-[12px] uppercase hover:text-red"
+                      aria-current={i === paths.length ? "page" : undefined}
+                    >
+                      {p}
+                    </Link>
+                  }
+                  {i == total &&
+                    <span className="text-[12px] uppercase font-semibold">{p}</span>
+                  }
                 </div>
               </li>
             );
