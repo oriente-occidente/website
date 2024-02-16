e26952bca16e8d910211e962520a65180b104fdb
Alberto De Stefani <orionlight231@gmail.com>
2024-02-16 17:16:30 +0100

    fix fallbackLocales

--- src/components/MyStructuredContent.js
+++ src/components/MyStructuredContent.js
@@ -15,12 +15,13 @@ const StructuredContent = ({ locale, content }) => {
   const renderBlock = (record) => {
     switch (record.__typename) {
       case "WorkshopCategoriesBlockRecord":
-        console.log("RECORD", record);
         return (
           <div className="py-6 lg:py-12 2xl:py-16" key={record.id}>
-            {record.tags.map(({ title, description, color, slug, overImage }, id) => {
+            {record.tags.map((t, id) => {
+              const { title, description, color, slug, overImage } = t || {};
+
               return (
-                <Link key={slug} href={`/studio/formazione/c/${slug}`} passHref>
+                <Link key={slug} href={resolveLink({ ...t, locale })} passHref>
                   <div
                     className={`group relative flex cursor-pointer flex-col overflow-hidden border-b  xl:flex-row xl:items-center ${
                       id === 0 && "border-t "
@@ -29,15 +30,14 @@ const StructuredContent = ({ locale, content }) => {
                     <div className={`z-40 flex items-center  pl-2`}>
                       <div className="flex items-center sm:flex-none ">
                         <div className="px-2 pl-8 pt-1 text-base sm:px-3 sm:pl-10 sm:pt-0 sm:text-lg lg:text-2xl xl:px-6 xl:pl-20 group-hover:xl:pl-6 relative z-[1] motion-safe:transition-all motion-safe:duration-500 ease-out">
-
-                          <div className="h-[18px] w-[18px] sm:h-[28px] sm:w-[28px] xl:h-[58px] xl:w-[58px] group-hover:w-[calc(100%+60px)] rounded-[100px] absolute left-0 top-1/2 -translate-y-1/2 z-[-1] motion-safe:transition-all motion-safe:duration-500 ease-out" style={{ backgroundColor: color?.hex || "#fff" }}>
-                          </div>
-
+                          <div
+                            className="h-[18px] w-[18px] sm:h-[28px] sm:w-[28px] xl:h-[58px] xl:w-[58px] group-hover:w-[calc(100%+60px)] rounded-[100px] absolute left-0 top-1/2 -translate-y-1/2 z-[-1] motion-safe:transition-all motion-safe:duration-500 ease-out"
+                            style={{ backgroundColor: color?.hex || "#fff" }}
+                          ></div>
                           {title.toUpperCase()}{" "}
                         </div>
                         <div className="ml-auto h-[28px] w-[28px] bg-arrow-small-right group-hover:bg-arrow-small-right-white lg:h-[38px] lg:w-[38px] xl:block motion-safe:transition-all motion-safe:duration-500 ease-out relative z-[1]" />
                       </div>
-
                     </div>
                     <div className="z-40 pt-3 pr-1 pl-2 text-xxs lg:w-[356px] xl:ml-auto xl:pt-0">
                       {description}
