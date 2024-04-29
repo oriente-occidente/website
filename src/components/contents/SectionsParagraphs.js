94d909cea0a94c8939057da0ad37a74482cd2245
Alessandro Donnini <info@alessandrodonnini.com>
2024-04-29 18:31:29 +0200

    Fixes

--- src/components/contents/SectionsParagraphs.js
+++ src/components/contents/SectionsParagraphs.js
@@ -4,16 +4,19 @@ import Link from "next/link";
 import StructuredContent from "@/components/MyStructuredContent";
 
 export default function SectionsParagraphs({ sections, locale, layout = "" }) {
+  const sectionsWithTitle = sections.filter((s) => s.title !== "");
   return (
     <div className="md:mx-auto md:container md:grid md:grid-cols-4 md:gap-4">
-      <nav className="hidden px-4 md:block">
-        <ul
-          className={`${layout == "no-space" ? "top-6" : "top-24"} sticky py-2`}
-        >
-          {sections.map((section) => (
-            <>
-              {section.title && (
-                <li key={section.id}>
+      {sectionsWithTitle.length > 0 && (
+        <nav className="hidden px-4 md:block">
+          <ul
+            className={`${
+              layout == "no-space" ? "top-6" : "top-24"
+            } sticky py-2`}
+          >
+            {sections.map((section) => (
+              <li key={section.id}>
+                {section.title && (
                   <Link
                     href={`#${section.id}`}
                     className={`${layout == "no-space" ? "" : "-top-20"}`}
@@ -23,17 +26,16 @@ export default function SectionsParagraphs({ sections, locale, layout = "" }) {
                       {section.title}
                     </span>
                   </Link>
-                </li>
-              )}
-            </>
-          ))}
-        </ul>
-      </nav>
-
+                )}
+              </li>
+            ))}
+          </ul>
+        </nav>
+      )}
       <div className="border-color-gray md:col-span-3 md:border-l">
         {sections.map((section) => (
           <Fragment key={section.id}>
-            <h2
+            <div
               className={`${
                 layout == "no-space" ? "-top-4" : "-top-20 xl:-top-32"
               } invisible relative block`}
