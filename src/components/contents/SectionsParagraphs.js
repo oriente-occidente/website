230304c7590ec042f5628687ad52ac71af319d9e
Alessandro Donnini <info@alessandrodonnini.com>
2024-04-29 18:22:38 +0200

    Fix anchor

--- src/components/contents/SectionsParagraphs.js
+++ src/components/contents/SectionsParagraphs.js
@@ -6,34 +6,34 @@ import StructuredContent from "@/components/MyStructuredContent";
 export default function SectionsParagraphs({ sections, locale, layout = "" }) {
   return (
     <div className="md:mx-auto md:container md:grid md:grid-cols-4 md:gap-4">
-      <div className="hidden px-4 md:block">
-        <div
+      <nav className="hidden px-4 md:block">
+        <ul
           className={`${layout == "no-space" ? "top-6" : "top-24"} sticky py-2`}
         >
           {sections.map((section) => (
             <>
               {section.title && (
-                <div key={section.id}>
+                <li key={section.id}>
                   <Link
                     href={`#${section.id}`}
                     className={`${layout == "no-space" ? "" : "-top-20"}`}
                     title={`Link to ${section.title}`}
                   >
-                    <h2 className="mb-1 text-xxs uppercase tracking-wider hover:text-red">
+                    <span className="mb-1 text-xxs uppercase tracking-wider hover:text-red">
                       {section.title}
-                    </h2>
+                    </span>
                   </Link>
-                </div>
+                </li>
               )}
             </>
           ))}
-        </div>
-      </div>
+        </ul>
+      </nav>
 
       <div className="border-color-gray md:col-span-3 md:border-l">
         {sections.map((section) => (
           <Fragment key={section.id}>
-            <div
+            <h2
               className={`${
                 layout == "no-space" ? "-top-4" : "-top-20 xl:-top-32"
               } invisible relative block`}
