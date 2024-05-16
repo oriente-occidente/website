e51bfdcdd7d0ebfe8796b7865c06bb5518c9acee
Alberto De Stefani <orionlight231@gmail.com>
2024-05-16 19:42:05 +0200

    wip3

--- src/app/(base)/forms/dev/page.tsx
+++ src/app/(base)/forms/dev/page.tsx
@@ -19,7 +19,36 @@ export default async function Page({ params, searchParams }: BasicSlugPageProps)
   const slugData = {};
   return (
     <Wrapper locale={locale} slugData={slugData}>
-      <DevForm />
+      <form name="contact" method="POST" data-netlify="true">
+        <input type="hidden" name="form-name" value="contact" />
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Role:{" "}
+            <select name="role[]" multiple>
+              <option value="leader">Leader</option>
+              <option value="follower">Follower</option>
+            </select>
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message"></textarea>
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     </Wrapper>
   );
 }
