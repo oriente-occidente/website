9966c6aa8f7c8588449f1f32895bfee1a949afc3
Alessandro Donnini <info@alessandrodonnini.com>
2024-05-27 17:54:16 +0200

    Update swiper lazy hero

--- src/components/hero/HeroSlider.js
+++ src/components/hero/HeroSlider.js
@@ -21,8 +21,12 @@ function HeroSlider({ slides }) {
         slidesPerView={1}
         pagination={true}
         navigation={true}
-        // preloadImages={true}
-        lazyload="true"
+        lazy={{
+          enabled: true,
+          loadPrevNext: true,
+          loadPrevNextAmount: 1,
+          loadOnTransitionStart: true,
+        }}
         className="mySwiper relative h-[40vh] md:h-[60vh] xl:h-[70vh]"
       >
         {slides.map((slide, i) => {
@@ -38,18 +42,6 @@ function HeroSlider({ slides }) {
                 priority={i == 0 ? true : false}
                 fetchpriority={i == 0 ? "high" : "low"}
               />
-              {/* <Image
-                className="h-full w-full duration-300 group-hover:scale-105"
-                src={image.url ? image.url : image}
-                alt={
-                  image.alt ? image.alt : "Oriente Occidente"
-                }
-                layout="fill"
-                objectFit="cover"
-                objectPosition="center"
-                priority={i == 0 ? true : false}
-                sizes={image.responsiveImage.sizes}
-              /> */}
               <div className="container relative h-full">
                 <h2 className="absolute bottom-16 right-4 z-20 w-4/5 text-right text-base font-normal text-white md:bottom-12 md:w-2/3 md:text-lg xl:w-1/2 xl:text-2xl 2xl:bottom-24 2xl:right-0">
                   {slide.title}
