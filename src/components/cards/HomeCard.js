c8d866908798e5a5734fe39747c3b9babbf03978
Alessandro Donnini <info@alessandrodonnini.com>
2024-05-30 09:43:36 +0200

    Home card

--- src/components/cards/HomeCard.js
+++ src/components/cards/HomeCard.js
@@ -15,7 +15,8 @@ function HomeCard({ count, categoryTitle, data, locale }) {
       aria-label={`Vai a ${data.title}`}
       className="group"
     >
-      <div className="relative h-[240px] pb-8 pl-4 md:h-[640px] md:pb-16 md:pl-6 lg:pb-20 xl:h-[45vw]">
+      {/* <div className="relative h-[240px] pb-8 pl-4 md:h-[640px] md:pb-16 md:pl-6 lg:pb-20 xl:h-[40vw]"> */}
+      <div className="relative pb-8 pl-4  md:pb-16 md:pl-6 lg:pb-20">
         <div className="relative flex h-full justify-between lg:pl-8 2xl:pl-[calc((100vw-1390px)/2)]">
           <div>
             <div className="relative text-xxs uppercase tracking-wider after:absolute after:top-7 after:left-0 after:h-px after:w-12 after:bg-black md:text-sm md:tracking-wide lg:text-base lg:after:top-12 lg:after:w-40">
@@ -25,16 +26,14 @@ function HomeCard({ count, categoryTitle, data, locale }) {
               {data.descriptionHero}
             </div>
           </div>
-          <div className="relative w-3/4 overflow-hidden lg:w-8/12">
+          <div className="relative w-3/4 overflow-hidden lg:w-8/12 aspect-[4/3] lg:aspect-square after:absolute after:inset-x-[-25%] after:inset-y-0 after:z-10 after:shadow-[0_0_150px_50px_rgba(0,0,0,0.7)_inset]">
             {data.imageHero ? (
               <DatoImage
-                // className="h-full w-full duration-300 group-hover:scale-105"
+                className="h-full w-full duration-300 group-hover:scale-105"
                 data={data.imageHero.responsiveImage}
-                // alt={data.imageHero.alt}
-                // title={data.imageHero.title}
-                // layout="fill"
-                // objectFit="cover"
-                // objectPosition="center"
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
                 priority={count == 1 ? true : false}
                 fetchpriority={count == 1 ? "high" : "low"}
               />
@@ -54,8 +53,6 @@ function HomeCard({ count, categoryTitle, data, locale }) {
               // />
               <div className="h-full w-full duration-300 group-hover:scale-105" />
             )}
-            <div className="absolute left-0 top-0 z-10 h-1/2 w-full bg-gradient-to-b from-black-transparent to-transparent"></div>
-            <div className="absolute bottom-0 left-0 right-0 z-10 hidden h-1/2 bg-gradient-to-t from-black-transparent to-transparent md:block"></div>
             <div className="absolute left-1/2 bottom-4 -translate-x-2/4 text-xl font-thin text-white after:absolute after:top-1/2 after:left-1/2 after:h-10 after:w-10 after:-translate-y-2/4 after:-translate-x-2/4 after:rounded-full after:border after:border-white md:hidden">
               +
             </div>
@@ -71,7 +68,8 @@ function HomeCard({ count, categoryTitle, data, locale }) {
           </h2>
         </div>
       </div>
-      <div className="title--small relative ml-20 mt-4 mb-12 pl-1 after:absolute after:-top-3 after:left-1 after:h-px after:w-32 after:bg-black md:hidden lg:py-16">
+      {/* <div className="title--small relative ml-20 mt-4 mb-12 pl-1 after:absolute after:-top-3 after:left-1 after:h-px after:w-32 after:bg-black md:hidden lg:py-16"> */}
+      <div className="title--small relative ml-20 mt-4 mb-12 pl-1g md:hidden lg:py-16">
         {data.descriptionHero}
       </div>
     </Link>
