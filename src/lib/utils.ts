b66a4806bf7470f925eb2e8f3da288534313a8a6
Alberto De Stefani <albidst@gmail.com>
2025-07-24 18:40:51 +0200

    fix dates timezone

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -145,8 +145,9 @@ export function formatDate(str: string, locale = "it", isDaily = false) {
 export function groupDatesByDay(dates: any[], locale = "it") {
   // dayjs.locale(locale);
   // console.log('DAYJS LOCALE', dayjs.locale(), locale);
+  console.log("DATES..", dates);
   const groups = dates.reduce((group, date) => {
-    const day = dayjs.utc(date.startTime).format("YYYY-MM-DD");
+    const day = dayjs(date.startTime).tz("Europe/Rome").format("YYYY-MM-DD");
     if (group[day]) {
       group[day].push(date);
     } else {
