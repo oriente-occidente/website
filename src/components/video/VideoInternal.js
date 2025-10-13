2eaf9264878c74cb04174b89e85a175f3aa79401
Alessandro Donnini <info@alessandrodonnini.com>
2025-10-13 10:11:24 +0200

    Update video player

--- src/components/video/VideoInternal.js
+++ src/components/video/VideoInternal.js
@@ -1,13 +1,47 @@
-const VideoInternal = (props) => {
-  const { id, videoFile, autoPlay, ...other } = props;
-  return (
-    <div key={id}>
-      <video autoPlay={autoPlay || false} {...other} style={{ width: "100%" }}>
-        <source src={videoFile?.url} type={videoFile?.mimeType} />
-        Sorry, your browser doesn`&apos;`t support embedded videos.
-      </video>
-    </div>
-  );
-};
+import "@vidstack/react/player/styles/base.css";
+import "@vidstack/react/player/styles/default/theme.css";
+import "@vidstack/react/player/styles/default/layouts/video.css";
+import { MediaPlayer, MediaProvider, Poster } from "@vidstack/react";
+import {
+  defaultLayoutIcons,
+  DefaultVideoLayout,
+} from "@vidstack/react/player/layouts/default";
+import Image from "next/image";
 
-export default VideoInternal;
+export default function VideoInternal(props) {
+  console.log("props", props);
+  const { internalVideo } = props;
+  const videoUrl = internalVideo?.video?.streamingUrl
+    ? internalVideo?.video?.streamingUrl
+    : internalVideo?.url;
+
+  // return;
+  return (
+    <MediaPlayer
+      autoPlay={false}
+      muted={true}
+      // playsInline={true}
+      paused={true}
+      controls={false}
+      controlsDelay="0"
+      loop={true}
+      title={internalVideo.title ? internalVideo.title : "Oriente Occidente"}
+      src={videoUrl}
+    >
+      <MediaProvider />
+
+      <Poster asChild className="vds-poster">
+        <Image
+          src={internalVideo.thumbnailUrl}
+          width={1280}
+          height={800}
+          alt={internalVideo.title ? internalVideo.title : "Italian Tech Week"}
+        />
+      </Poster>
+      <DefaultVideoLayout
+        thumbnails={internalVideo.thumbnailUrl}
+        icons={defaultLayoutIcons}
+      />
+    </MediaPlayer>
+  );
+}
